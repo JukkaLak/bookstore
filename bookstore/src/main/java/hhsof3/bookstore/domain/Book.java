b14a3e7c2a0b84a63a4692954ddd817be71258ff
Jukka Lakkala <bgx205@myy.haaga-helia.fi>
2023-02-04 11:07:04 +0200

    First commit

---
+++ bookstore/src/main/java/hhsof3/bookstore/domain/Book.java
@@ -0,0 +1,76 @@
+package hhsof3.bookstore.domain;
+
+public class Book {
+	
+	private String title;
+	private String author;
+	private Integer year;
+	private Integer isbn;
+	private Double price;
+	
+	public Book() {
+		super();
+		this.title = null;
+		this.author = null;
+		this.year = null;
+		this.isbn = null;
+		this.price = null;
+	}
+	
+	public Book(String title, String author, Integer year, Integer isbn, Double price) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.isbn = isbn;
+		this.price = price;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public void setIsbn(Integer isbn) {
+		this.isbn = isbn;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public Integer getIsbn() {
+		return isbn;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn + ", price=" + price
+				+ "]";
+	}
+	
+
+}
---
+++ bookstore/src/main/java/hhsof3/bookstore/web/BookstoreController.java
@@ -0,0 +1,26 @@
+package hhsof3.bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+
+@Controller
+@ResponseBody
+public class BookstoreController {
+	
+	@GetMapping("/index")
+	public String index() {
+		return "Bookstore";
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
