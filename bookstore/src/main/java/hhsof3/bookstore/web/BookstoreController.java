5d1820b962e6960a027b29e3aab257c32d75e91b
Jukka Lakkala <bgx205@myy.haaga-helia.fi>
2023-02-12 11:38:49 +0200

    Update 2

--- bookstore/src/main/java/hhsof3/bookstore/web/BookstoreController.java
+++ bookstore/src/main/java/hhsof3/bookstore/web/BookstoreController.java
@@ -1,9 +1,18 @@
 package hhsof3.bookstore.web;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import hhsof3.bookstore.domain.Book;
+import hhsof3.bookstore.domain.BookRepository;
+
 
 
 
@@ -17,6 +26,22 @@ public class BookstoreController {
 	public String index() {
 		return "welcome-template";
 	}
+	
+	@Autowired
+	
+	BookRepository bookRepository;
+	
+	@RequestMapping(value = "/books", method = RequestMethod.GET)
+	public String getBooks(Model model) {
+		
+		List<Book> books = (List<Book>) bookRepository.findAll();
+		
+		model.addAttribute("books", books);
+		
+		return "book-list";
+	}
+	
+
 	
 	
 	
