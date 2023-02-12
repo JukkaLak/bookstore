14454d27743ceda02f6fbacf250c02d64818f13c
Jukka Lakkala <bgx205@myy.haaga-helia.fi>
2023-02-12 14:27:05 +0200

    Update 3

--- bookstore/src/main/java/hhsof3/bookstore/web/BookstoreController.java
+++ bookstore/src/main/java/hhsof3/bookstore/web/BookstoreController.java
@@ -3,10 +3,12 @@ package hhsof3.bookstore.web;
 
 import java.util.List;
 
+import org.apache.catalina.startup.ClassLoaderFactory.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -41,6 +43,23 @@ public class BookstoreController {
 		return "book-list";
 	}
 	
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Book book) {
+		bookRepository.save(book);
+		return "redirect:books";
+	}
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		bookRepository.deleteById(bookId);
+		return "redirect:../books";
+	}
 
 	
 	
