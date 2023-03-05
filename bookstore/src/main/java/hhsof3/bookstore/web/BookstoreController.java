68ceef25d945104b8a0f6f444a7749c9365552d5
Jukka Lakkala <bgx205@myy.haaga-helia.fi>
2023-03-05 15:15:51 +0200

    Update 5

--- bookstore/src/main/java/hhsof3/bookstore/web/BookstoreController.java
+++ bookstore/src/main/java/hhsof3/bookstore/web/BookstoreController.java
@@ -11,6 +11,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -57,6 +59,11 @@ public class BookstoreController {
 	@GetMapping("/books/{id}")
 	public @ResponseBody Optional<Book> getOneBook(@PathVariable("id") Long bookId){
 		return repository.findById(bookId);
+	}
+	
+	@PostMapping("/books")
+	public @ResponseBody Book addNewBook(@RequestBody Book book) {
+		return repository.save(book);
 	}
 	
 	
